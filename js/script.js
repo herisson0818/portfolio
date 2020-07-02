f95a51fabdf07c8d109abe59932d9d95d471446d
坪田健人 <lyla.0218@gmail.com>
2020-07-03 07:00:42 +0900

    カウント

--- js/script.js
+++ js/script.js
@@ -46,3 +46,35 @@ $(function () {
     return false;
   });
 });
+
+var countdown = function(due) {
+	var now = new Date();
+
+	var rest = due.getTime() - now.getTime();
+	var sec = Math.floor(rest / 1000 % 60);
+	var min = Math.floor(rest / 1000 / 60) % 60;
+	var hours = Math.floor(rest /1000 / 60 /60) % 24;
+	var days = Math.floor(rest / 1000 / 60 / 60 / 24);
+	var count = [days, hours, min, sec];
+
+	return count;
+}
+
+var goal = new Date(2021, 1, 1);
+goal.setHours(23);
+goal.setMinutes(59)
+goal.setSeconds(59);
+
+var recalc = function() {
+	var counter = countdown(goal);
+	document.getElementById('day').textContent = counter[0];
+	document.getElementById('hour').textContent = counter[1];
+	document.getElementById('min').textContent = counter[2];
+	document.getElementById('sec').textContent = counter[3];
+	refresh();
+}
+
+var refresh = function() {
+	setTimeout(recalc, 1000);
+}
+recalc();
